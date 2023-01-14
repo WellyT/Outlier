5df35de8638a5d2b10b72f01aef309cd0e78e05b
WellyT <wellysonthomas@hotmail.com>
2023-01-14 14:40:58 -0300

    Added flex wrap to the team section

---
+++ index.js
@@ -0,0 +1,18 @@
+function onScroll() {
+    let nav = document.getElementById('navigation')
+    if (scrollY > 0) {
+   
+        nav.classList.add('scroll')
+    }else{
+        nav.classList.remove('scroll')
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
